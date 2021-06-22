ce829638401ac2f24fad5efda37e860949170950
Deep1Shikha <shikhadtg2011@gmail.com>
2021-06-22 20:44:10 +0530

    footer created

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,42 @@
 export default function footer() {
-    return <footer className="footer">Footer</footer>;
-  }
-  
+  return (
+    <footer className="footer">
+      <ul className="nav">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Request Proposal
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Download Home Planner
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact Us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            List your Property
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Careers
+          </a>
+        </li>
+        
+      </ul>
+      <p className="copyright">
+              &copy;Deep Shikha
+        </p>
+    </footer>
+  );
+}
